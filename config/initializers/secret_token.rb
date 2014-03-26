c8e3214e4a8724bf83f0531e44834790ffd534ce
Nathan Hall <nathan.mh@gmail.com>
2014-03-26 15:51:47 -0500

    initialized application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YourOwnStagram::Application.config.secret_key_base = 'e0c443440a965e4f934dfb11907cdcb4f5c7b3cdb8edf64da01159830c458f25c0add528f245bf93306295f4fa56b849577ad2cc9d977bbcbd1069af15bda0ea'
